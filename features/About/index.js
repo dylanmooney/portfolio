7a0ec3d5fb4b7b70579eb6e55677307d81f9e2b4
Dylan Mooney <dylan@dylanmooney.co.uk>
2022-09-04 20:59:21 +0100

    Fix text align

--- features/About/index.js
+++ features/About/index.js
@@ -6,34 +6,37 @@ import { Skills } from './Skills';
 import about from './about.json';
 
 export const About = () => {
-	const { description, skills } = about;
+  const { description, skills } = about;
 
-	return (
-		<Element name='about'>
-			<Box
-				clipPath='polygon(0 10%, 100% 0%, 100% 90%, 0% 100%)'
-				pt={{ base: 24, md: 28, lg: 32, xl: 44 }}
-				pb={{ base: 24, sm: 32, md: 36, xl: 52 }}>
-				<Overlay zIndex={-10} />
-				<Container variant='section' pos='relative'>
-					<Box as='h2' textStyle='h2' textAlign={{ lg: 'left' }}>
-						About Me
-					</Box>
-					<Box display={{ lg: 'flex' }} gap={7} justifyContent='space-between'>
-						<Box flex={1} mb={8}>
-							{description.split('\n').map((text, idx) => (
-								<Text key={idx} textStyle='p'>
-									{text}
-								</Text>
-							))}
-						</Box>
+  return (
+    <Element name="about">
+      <Box
+        clipPath="polygon(0 10%, 100% 0%, 100% 90%, 0% 100%)"
+        pt={{ base: 24, md: 28, lg: 32, xl: 44 }}
+        pb={{ base: 24, sm: 32, md: 36, xl: 52 }}>
+        <Overlay zIndex={-10} />
+        <Container variant="section" pos="relative">
+          <Box
+            as="h2"
+            textStyle="h2"
+            textAlign={{ base: 'center', lg: 'left' }}>
+            About Me
+          </Box>
+          <Box display={{ lg: 'flex' }} gap={7} justifyContent="space-between">
+            <Box flex={1} mb={8}>
+              {description.split('\n').map((text, idx) => (
+                <Text key={idx} textStyle="p">
+                  {text}
+                </Text>
+              ))}
+            </Box>
 
-						<Box flex={1}>
-							<Skills skills={skills} />
-						</Box>
-					</Box>
-				</Container>
-			</Box>
-		</Element>
-	);
+            <Box flex={1}>
+              <Skills skills={skills} />
+            </Box>
+          </Box>
+        </Container>
+      </Box>
+    </Element>
+  );
 };
